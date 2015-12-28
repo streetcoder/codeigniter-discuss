b8b7428be58d409bf39e17abe4a9afc4a7603265
streetcoder <proshimul@yahoo.com>
2015-12-28 09:38:30 +0600

    added new comment

--- application/models/Comments_model.php
+++ application/models/Comments_model.php
@@ -34,13 +34,40 @@ class Comments_model extends CI_Model {
      * */
     public function newComment($data){
 
-        //$comment_data = $data;
+        $usr_email = $data['usr_email'];
+        $query = "SELECT * FROM `users` WHERE `usr_email` = ? ";
+        $result = $this->db->query($query,array($usr_email));
+
+        if ($result->num_rows() > 0) {
+            foreach ($result->result() as $rows) {
+                $data['usr_id'] = $rows->usr_id;
+            }
+        } else {
+            $password = random_string('alnum', 16);
+
+            $hash = password_hash($password, PASSWORD_BCRYPT);
+
+            $user_data = array('usr_email' => $data['usr_email'],
+                'usr_name' => $data['usr_name'],
+                'usr_is_active' => '1',
+                'usr_level' => '1',
+                'usr_hash' => $hash);
+
+            if ($this->db->insert('users',$user_data)) {
+                $data['usr_id'] = $this->db->insert_id();
+            }
+        }
+
         $comment_data = array('cm_body' => $data['cm_body'],
             'ds_id' => $data['ds_id'],
             'cm_is_active' => '1',
-            'usr_id' => 2);
+            'usr_id' => $data['usr_id']);
 
-        $this->db->insert('comments',$comment_data);
+        if ($this->db->insert('comments',$comment_data) ) {
+            return $this->db->insert_id();
+        } else {
+            return false;
+        }
     }
 
 }
